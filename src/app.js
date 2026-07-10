ca50d0c763e596bbe6a6c55ce23c973f840f0fd2
Himadri Ghosh <himadrighosh2103@gmailcom>
2026-07-11 02:52:09 +0530

    Add Routes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ const noteModel = require('./models/note.model');
 const app = express();
 app.use(express.json());
 
+app.post('/notes', async (req, res)=>{
+  const data = req.body;
 
+  await noteModel.create({
+    title: data.title,
+    description: data.description
+  });
 
+  res.status(201).json({
+    message: "Notes created successfully",
+  })
+});
+
+app.get('/notes', async (req, res)=>{
+  const notes = await noteModel.find();
+
+  res.status(201).json({
+    message: "notes fetched successfully",
+    notes
+  })
+});
+
+app.delete('/notes/:id', async (req, res)=>{
+  const id = req.params.id;
+
+  await noteModel.findOneAndDelete({
+    _id: id
+  });
+
+  res.status(201).json({
+    message: "Note deleted successfully"
+  })
+});
+
+app.patch('/notes/:id', async (req, res)=>{
+  const id = req.params.id;
+
+  if(req.body.description !== undefined){
+    await userModel.findOneAndUpdate({ _id:id }, { description: req.body.description });
+  }
+  if(req.body.title !== undefined){
+    await userModel.findOneAndUpdate({ _id:id }, { title: req.body.title });
+  }
+
+  res.status(201).json({
+    message: "Notes updated successfully"
+  })
+});
 
 module.exports = app;
